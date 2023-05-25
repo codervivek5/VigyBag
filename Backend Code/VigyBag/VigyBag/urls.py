5fc87aded4b261bb7540f559889690a604731202
codervivek <62784060+codervivek5@users.noreply.github.com>
2023-05-25 14:59:42 +0530

    add backend code first time

--- Backend Code/VigyBag/VigyBag/urls.py
+++ Backend Code/VigyBag/VigyBag/urls.py
@@ -1,7 +1,7 @@
-"""Backend URL Configuration
+"""VigyBag URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,9 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("",include('home.urls'))
+    path('admin/', admin.site.urls),
 ]
