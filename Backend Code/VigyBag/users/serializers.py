9cc6df292dfeb828a865d7cbd4016e4468f3e3af
sanya-mahajan <sanyamahajan08@gmail.com>
2023-05-27 06:55:35 +0530

    shift crud to views

--- Backend Code/VigyBag/users/serializers.py
+++ Backend Code/VigyBag/users/serializers.py
@@ -2,42 +2,10 @@ from rest_framework import serializers
 from .models import CustomUser
 from django.contrib.auth import authenticate
 
-class RegisterUserSerializer(serializers.ModelSerializer):
-    email=serializers.EmailField(max_length=255)
-    password=serializers.CharField(max_length=128,write_only=True)
-
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = CustomUser
-        fields = ('email', 'password')  
-        #have to add usertype ,default customer
+        model=CustomUser
+        fields=['id','email','password','user_type','last_login']
     
-    def create(self, validated_data):
-        user = CustomUser.objects.create_user(email=validated_data['email'], password=validated_data['password'])
-        return user
-    
-class LoginSerializer(serializers.ModelSerializer):
-    email=serializers.EmailField(max_length=255)
-    password=serializers.CharField(max_length=128,write_only=True)
-    class Meta:
-        model = CustomUser
-        fields = ('email', 'password')
-
-    def validate(self, data):
-        email=data.get('email', None)
-        password=data.get('password', None)
-        if email and password:
-            user=authenticate(email=email, password=password)
-            print(1234)
-            if user:
-                data['user']=user
-
-                return data
-
-            else:
-                raise serializers.ValidationError('Incorrect Credentials')
-            
-
-        else:
-            raise serializers.ValidationError('Must provide email and password')
              
               
--- Backend Code/VigyBag/users/views.py
+++ Backend Code/VigyBag/users/views.py
@@ -12,27 +12,34 @@ from rest_framework.authentication import TokenAuthentication
 class RegisterView(APIView):
     def post(self, request):
         email=request.data['email']
+        password=request.data['password']
+        if not email or not password:
+            return Response({'status':'failed','message':'Please provide both email and password'},status=status.HTTP_400_BAD_REQUEST)
+        
+        
         if CustomUser.objects.filter(email=email).exists():
             return Response({'status':'failed','message':'Email already exists'},status=status.HTTP_400_BAD_REQUEST)
-        serializer = RegisterUserSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            new_user = CustomUser.objects.get(email=serializer.data['email'])
-            token,_ = Token.objects.get_or_create(user=new_user)
-            return Response({'status':'success','data':serializer.data,'token':str(token)},status=status.HTTP_201_CREATED)
-            
-        else:
-            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        
+        new_user=CustomUser.objects.create_user(email=email,password=password)
+        new_user.save()
+        token,_ = Token.objects.get_or_create(user=new_user)
+        serializer=UserSerializer(new_user)
+        return Response({'status':'success','data':serializer.data,'token':str(token)},status=status.HTTP_201_CREATED)
+        
 
 #allow posting to login view to get token
 
 class LoginView(APIView):
     def post(self,request):
-        serializer = LoginSerializer(data=request.data)
-        if serializer.is_valid():
-            print(1)
-            user = serializer.validated_data['user']
-            token,_ = Token.objects.get_or_create(user=user)
-            return Response({'status':'success','data':serializer.data,'token':str(token)},status=status.HTTP_200_OK)
-        else:
-            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        email=request.data['email']
+        password=request.data['password']
+        if not email or not password:
+            return Response({'status':'failed','message':'Please provide both email and password'},status=status.HTTP_400_BAD_REQUEST)
+        user=authenticate(email=email, password=password)
+        if not user:
+            return Response({'status':'failed','message':'Invalid Credentials'},status=status.HTTP_404_NOT_FOUND)
+        
+        token,_ = Token.objects.get_or_create(user=user)
+        serializer=UserSerializer(user)
+        return Response({'status':'success','data':serializer.data,'token':str(token)},status=status.HTTP_200_OK)
+    
