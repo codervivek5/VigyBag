e412489da5f27e98869ecff4a4a871009ad41955
“Esther <essyc14@gmail.com>
2023-07-07 00:20:17 +0300

    Changes made

--- Android/app/src/main/java/com/vigbag/android/MainActivity.kt
+++ Android/app/src/main/java/com/vigbag/android/MainActivity.kt
@@ -2,6 +2,7 @@ package com.vigbag.android
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.fragment.app.Fragment
 import com.vigbag.android.databinding.ActivityMainBinding
 
@@ -9,6 +10,8 @@ class MainActivity : AppCompatActivity() {
     lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Thread.sleep(200)
+        installSplashScreen()
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
 
