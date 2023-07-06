2b7ce3677f331dcfe45d9b5e134b57f108a55d2b
Aman Nishad <amannishad0512@gmail.com>
2023-07-06 17:16:07 +0530

    Navigation Graph issues fixed

--- Android/app/src/main/java/com/vigbag/android/MainActivity.kt
+++ Android/app/src/main/java/com/vigbag/android/MainActivity.kt
@@ -2,15 +2,15 @@ package com.vigbag.android
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 import com.vigbag.android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setTheme(R.style.Theme_VigyBag)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
-
-
 }
 
--- Android/app/src/main/java/com/vigbag/android/SignUpFragment.kt
+++ Android/app/src/main/java/com/vigbag/android/SignUpFragment.kt
@@ -2,12 +2,10 @@ package com.vigbag.android
 
 import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import com.vigbag.android.databinding.FragmentSignUpBinding
 
 class SignUpFragment : Fragment(R.layout.fragment_sign_up) {
