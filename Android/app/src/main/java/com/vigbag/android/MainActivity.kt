756d4110dcb564461504181b3ac5404e6f1e4205
“Esther <essyc14@gmail.com>
2023-07-06 23:15:46 +0300

    Splash screen

--- Android/app/src/main/java/com/vigbag/android/MainActivity.kt
+++ Android/app/src/main/java/com/vigbag/android/MainActivity.kt
@@ -10,6 +10,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+
     }
 
 
