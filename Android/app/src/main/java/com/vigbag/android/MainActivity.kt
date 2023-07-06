8804eadf8d3f09c70395dab303a750e07bbf03be
Jasjeet Singh <98077881+07jasjeet@users.noreply.github.com>
2023-07-06 14:37:43 +0530

    Commit suggestion

--- Android/app/src/main/java/com/vigbag/android/MainActivity.kt
+++ Android/app/src/main/java/com/vigbag/android/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.fragment.app.Fragment
 import com.vigbag.android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
