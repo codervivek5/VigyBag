e19acf746934397d3a3dda391674b1a932212dbb
Aman Nishad <amannishad0512@gmail.com>
2023-07-06 17:56:01 +0530

    Navigation Graph MainActivity Issue Fixed

--- Android/app/src/main/java/com/vigbag/android/MainActivity.kt
+++ Android/app/src/main/java/com/vigbag/android/MainActivity.kt
@@ -2,15 +2,13 @@ package com.vigbag.android
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.vigbag.android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setTheme(R.style.Theme_VigyBag)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
     }
+
+
 }
 
