d28579d266cfa0db5fba05fdd020992828ff3386
Ojaswi18 <you@example.comthakareojaswi@gmail.com>
2023-07-15 23:32:16 +0530

    code added for twitter backend

--- Backend Code/VigyBag/VigyBag/settings.py
+++ Backend Code/VigyBag/VigyBag/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.facebook',
     'allauth.socialaccount.providers.google',
     'allauth.socialaccount.providers.twitter_oauth2',
+    "Contact",
 ]
 
 REST_FRAMEWORK = {
