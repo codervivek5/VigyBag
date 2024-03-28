114bfa83cffabb83e760ef503b74fa89e1624f3f
Vivek Prajapati <62784060+codervivek5@users.noreply.github.com>
2024-03-28 19:25:31 +0530

    fix navigation menu issue - related to day and night toggle button

--- script/tempCodeRunnerFile.js
+++
@@ -1,6 +0,0 @@
-burgerIcon.addEventListener("click", () => {
-//   console.log("Hello");
-//   burgerIcon.classList.toggle("fa-bars");
-//   burgerIcon.classList.toggle("fa-xmark");
-//   navMenu.classList.toggle("show");
-// });
