5c2593b61a0ea9f58930104939e9245dce2a4ed2
Vivek Prajapati <62784060+codervivek5@users.noreply.github.com>
2024-05-31 15:08:33 +0530

    added logo and fix the routing (remove <a> tag and replce to <NavLink>

--- Front-with-React/VigyBag/src/components/Navbar/Navbar.jsx
+++ Front-with-React/VigyBag/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { FaSearch, FaShoppingCart, FaBell, FaUserCircle } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { Link , NavLink} from 'react-router-dom';
 
+import logo from '../../assets/images/Logo.svg'
 
 
 // ...
@@ -13,31 +14,36 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className="flex  items-center justify-between bg-[#fdf1e8] p-4">
+    <nav className="flex  items-center justify-between bg-[#fdf1e8] p-3">
 
       {/* Logo and Navigation Links */}
-      <div className="flex  gap-10 items-center">
-        <img src="" alt="Logo" className="h-10" />
+      <div className="flex  gap-5 items-center">
+
+      <NavLink to="/">
+        <img src={logo} alt="Logo" className="  h-10" />
+      </NavLink>
+
         <div className="ml-4">
           {/* Home Link */}
-          <a href="/" className="mx-2 text-black text-2xl font-bold">Home</a>
+          <NavLink to="/" className="mx-2 text-black text-2xl font-bold ">Home</NavLink>
 
           {/* Gifts Dropdown */}
           <div className="inline-block relative">
-            <a href="/gifts" className="mx-2 text-2xl text-black font-bold">Gifts</a>
+            <NavLink to="/gifts" className="mx-2 text-2xl text-black font-bold">Gifts</NavLink>
           </div>
 
           {/* Categories Dropdown */}
           <div className="inline-block relative">
-            <a href="/categories" className="mx-2 text-2xl text-black font-bold">Categories</a>
+            <NavLink to="/categories" className="mx-2 text-2xl text-black font-bold">Categories</NavLink>
           </div>
 
           {/* About Us Link */}
-          <a href="/about" className="mx-2 text-2xl text-black font-bold">About Us</a>
+          <NavLink to="/about" className="mx-2 text-2xl text-black font-bold">About Us</NavLink>
         </div>
       </div>
+
       {/* Search Bar */}
-      <div className="flex  gap-10 items-center  rounded-full border-black border-2  bg-gray-200 px-4 py-1">
+      <div className="flex  gap-10 items-center  rounded-full border-black border-2  bg-gray-200 px-4 py-2">
         <input type="text" placeholder="Search" className="bg-transparent outline-none" />
         <FaSearch className="text-gray-600" />
       </div>
--- Front-with-React/VigyBag/src/pages/gifts/Gifts.jsx
+++ Front-with-React/VigyBag/src/pages/gifts/Gifts.jsx
@@ -1,10 +1,8 @@
 import React from "react";
-import Navbar from "../../components/Navbar/Navbar";
 
 function Gifts() {
     return (
         <div>
-            <Navbar />
             <h1>Gifts</h1>
         </div>
     );
