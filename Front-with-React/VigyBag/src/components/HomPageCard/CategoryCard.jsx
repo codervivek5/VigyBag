d556a6919d3d261de02039e7e2c1116694527dc1
Lucifer_26 <138283976+Tanmay-Mirgal@users.noreply.github.com>
2024-06-23 21:49:55 +0530

    Home Page is Done

--- Front-with-React/VigyBag/src/components/HomPageCard/CategoryCard.jsx
+++ Front-with-React/VigyBag/src/components/HomPageCard/CategoryCard.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 
-const CategoryCard = ({ imageUrl, categoryName }) => {
+const CategoryCard = ({ image, name }) => {
   return (
     <div 
       className="relative w-60 h-64 mx-auto flex items-center justify-center"
       style={{
-        backgroundImage: `url(${imageUrl})`,
-        backgroundSize: '241px 250px',
+        backgroundImage: `url(${image})`,
+        backgroundSize: 'cover',
         backgroundPosition: 'center',
         backgroundRepeat: 'no-repeat',
       }}
     >
-      <div className="bg-black bg-opacity-75 text-white px-6 py-2 rounded-lg" style={{ width: '300px', height: '66px' }}>
-        <h3 className="text-lg  flex justify-center align-middle text-center font-semibold">{categoryName}</h3>
+      <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-75 text-white px-4 py-2 rounded-b-lg">
+        <h3 className="text-lg font-semibold text-center">{name}</h3>
       </div>
     </div>
   );
