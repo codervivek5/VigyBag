d1eed3303d1645d78b005bcdfaec1e174a0bcbc4
Vivek Prajapati <62784060+codervivek5@users.noreply.github.com>
2024-06-24 23:24:47 +0530

    New version of VigyBag - 1.0.0

--- Pages/privacypolicy/privacy.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const lastUpdatedElement = document.querySelector('header p');
-    const lastUpdatedDate = new Date().toLocaleDateString();
-    lastUpdatedElement.textContent = `Last updated: ${lastUpdatedDate}`;
-});
