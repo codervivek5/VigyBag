edaa4e819b84ae9911826ac64e0b82493d43512b
Lenovo <vishwayatra919@gmail.com>
2024-06-27 23:32:41 +0530

    initial commit for android

---
+++ Android/VigyBag/app/src/main/java/com/example/vigybag/activity/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.vigybag.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.vigybag.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+    }
+}
---
+++ Android/VigyBag/app/src/main/java/com/example/vigybag/models/UserModel.kt
@@ -0,0 +1,7 @@
+package com.example.vigybag.models
+
+data class UserModel(
+    val name:String?=null,
+    val email:String?=null
+
+)
