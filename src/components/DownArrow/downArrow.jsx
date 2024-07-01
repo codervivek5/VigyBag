d4ab7bae3df7e635f46714ef4b72b52954f2d11f
Sadaf <137484958+SadafKausar2025@users.noreply.github.com>
2024-07-01 17:44:34 +0530

    removed the functionality

--- src/components/DownArrow/downArrow.jsx
+++ src/components/DownArrow/downArrow.jsx
@@ -3,13 +3,6 @@ import "../../pages/Home/Home.css";
 import Arrow from "../../assets/arrow.png"; // Make sure to update the path to your actual image location
 
 const DownArrow = () => {
-  const scrollToBottom = () => {
-    window.scrollTo({
-      top: document.body.scrollHeight,
-      behavior: "smooth",
-    });
-  };
-
   return (
     <div className="down-arrow flex justify-center items-center">
       <img
@@ -17,7 +10,6 @@ const DownArrow = () => {
         src={Arrow}
         alt="Down Arrow"
         id="arrow"
-        onClick={scrollToBottom}
       />
     </div>
   );
