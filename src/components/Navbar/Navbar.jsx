e0e5018ca78547d6fca86b152537e91c92c5d2e5
amandeeep02 <amandeepsr02@gmail.com>
2024-07-04 00:28:27 +0530

    Fixed Navbar responsiveness

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
 
   return (
     <nav className="bg-[#ecd5c5] shadow-md">
-      <div className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-8xl mx-auto px-3 sm:px-2 lg:px-2">
         <div className="flex justify-between h-20">
           <div className="flex items-center">
             <NavLogo />
