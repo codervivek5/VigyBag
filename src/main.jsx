2a65163cdb7e37d04c820233b8102276a1fc73a6
Sadaf <137484958+SadafKausar2025@users.noreply.github.com>
2024-07-06 13:59:55 +0530

    Added Terms and Condition page in vigyBag

--- src/main.jsx
+++ src/main.jsx
@@ -38,20 +38,21 @@ import Cancellation from "./pages/Cancellation/Cancellation";
 import Payment from "./pages/Payment/Payment";
 import ReturnAndCancellation from "./pages/ReturnAndCancellation/returnAndCancellation";
 import EPRPage from "./pages/EPRPage/EPR_Page";
+import TermsAndConditions from "./pages/termsAndCondition/termsAndCondition";
 
 // Product pages
 import BambooProducts from "./pages/Latest_in_the_Market/BambooProducts";
 
 // Popular Categories
 import PopularCategories from "./pages/Popular_Categories/Popular_Categories";
-import BeautyWellness from './pages/Popular_Categories/Beauty-Wellness';
-import BodyCare from './pages/Popular_Categories/Body-Care';
-import CustomizedGifts from './pages/Popular_Categories/Customized-Gifts';
-import FashionAccessories from './pages/Popular_Categories/Fashion-Accessories';
-import FoodBeverages from './pages/Popular_Categories/Food-Beverages';
-import FurnitureDecor from './pages/Popular_Categories/Furniture-Decor';
-import HealthSupplements from './pages/Popular_Categories/Health-Supplements';
-import PrintingStationery from './pages/Popular_Categories/Printing-Stationery';
+import BeautyWellness from "./pages/Popular_Categories/Beauty-Wellness";
+import BodyCare from "./pages/Popular_Categories/Body-Care";
+import CustomizedGifts from "./pages/Popular_Categories/Customized-Gifts";
+import FashionAccessories from "./pages/Popular_Categories/Fashion-Accessories";
+import FoodBeverages from "./pages/Popular_Categories/Food-Beverages";
+import FurnitureDecor from "./pages/Popular_Categories/Furniture-Decor";
+import HealthSupplements from "./pages/Popular_Categories/Health-Supplements";
+import PrintingStationery from "./pages/Popular_Categories/Printing-Stationery";
 
 // Admin
 import AdminLogin from "./Admin/AdminLogin";
@@ -61,21 +62,42 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route index element={<Home />} />
-      
+
       {/* Admin routes */}
       <Route path="admin" element={<AdminLogin />} />
       <Route path="loggedin" element={<LoggedInContent />} />
 
       {/* Popular Categories */}
       <Route path="popularCategories" element={<PopularCategories />} />
-      <Route path="popularCategories/beautyWellness" element={<BeautyWellness />} />
+      <Route
+        path="popularCategories/beautyWellness"
+        element={<BeautyWellness />}
+      />
       <Route path="popularCategories/bodyCare" element={<BodyCare />} />
-      <Route path="popularCategories/customizedGifts" element={<CustomizedGifts />} />
-      <Route path="popularCategories/fashionAccessories" element={<FashionAccessories />} />
-      <Route path="popularCategories/foodBeverages" element={<FoodBeverages />} />
-      <Route path="popularCategories/furnitureDecor" element={<FurnitureDecor />} />
-      <Route path="popularCategories/healthSupplements" element={<HealthSupplements />} />
-      <Route path="popularCategories/printingStationery" element={<PrintingStationery />} />
+      <Route
+        path="popularCategories/customizedGifts"
+        element={<CustomizedGifts />}
+      />
+      <Route
+        path="popularCategories/fashionAccessories"
+        element={<FashionAccessories />}
+      />
+      <Route
+        path="popularCategories/foodBeverages"
+        element={<FoodBeverages />}
+      />
+      <Route
+        path="popularCategories/furnitureDecor"
+        element={<FurnitureDecor />}
+      />
+      <Route
+        path="popularCategories/healthSupplements"
+        element={<HealthSupplements />}
+      />
+      <Route
+        path="popularCategories/printingStationery"
+        element={<PrintingStationery />}
+      />
 
       {/* Other routes */}
       <Route path="about" element={<AboutUs />} />
@@ -99,7 +121,8 @@ const router = createBrowserRouter(
       <Route path="EPR_Page" element={<EPRPage />} />
       <Route path="faq" element={<FAQ />} />
       <Route path="CartEmpty" element={<CartEmpty />} />
-      
+      <Route path="termsAndCondition" element={<TermsAndConditions />} />
+
       {/* 404 route */}
       <Route path="*" element={<Error />} />
     </Route>
@@ -107,6 +130,4 @@ const router = createBrowserRouter(
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <RouterProvider router={router} />
-);
+root.render(<RouterProvider router={router} />);
