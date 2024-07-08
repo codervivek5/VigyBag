a2d8c505208e368c999fcdf8273899b8a58abd95
Sadaf <137484958+SadafKausar2025@users.noreply.github.com>
2024-07-08 17:54:18 +0530

    resolve conflicts

--- src/components/Navbar/MobileMenu.jsx
+++ src/components/Navbar/MobileMenu.jsx
@@ -9,49 +9,40 @@ const MobileMenu = ({ isOpen, isLoggedIn, handleLogout, isClose }) => (
       <div className="py-1">
         <Link
           to="popularCategories/fashionAccessories"
-          className="text-black hover:text-gray-600 block px-4 py-2 text-sm"
-          onClick={isClose}>
+          className="text-black hover:text-gray-600 block px-4 py-2 text-sm">
           Fashion
         </Link>
         <Link
           to="popularCategories/customizedGifts"
-          className="text-black hover:text-gray-600 block px-4 py-2 text-sm"
-          onClick={isClose}>
+          className="text-black hover:text-gray-600 block px-4 py-2 text-sm">
           Gifts
         </Link>
         <Link
           to="popularCategories/furnitureDecor"
-          className="text-black hover:text-gray-600 block px-4 py-2 text-sm"
-          onClick={isClose}>
+          className="text-black hover:text-gray-600 block px-4 py-2 text-sm">
           Furniture
         </Link>
         <Link
           to="popularCategories/printingStationery"
-          className="text-black hover:text-gray-600 block px-4 py-2 text-sm"
-          onClick={isClose}>
+          className="text-black hover:text-gray-600 block px-4 py-2 text-sm">
           Stationary
         </Link>
         <Link
           to="popularCategories/bodyCare"
-          className="text-black hover:text-gray-600 block px-4 py-2 text-sm"
-          onClick={isClose}>
+          className="text-black hover:text-gray-600 block px-4 py-2 text-sm">
           Body-Care
         </Link>
       </div>
       <Link
         to="/cart"
-        className="text-black hover:text-gray-600 px-3 py-2 rounded-md text-lg font-medium flex items-center"
-        onClick={isClose}>
+        className="text-black hover:text-gray-600 px-3 py-2 rounded-md text-lg font-medium flex items-center">
         <FaShoppingCart className="mr-2" />
         Cart
       </Link>
 
       {isLoggedIn ? (
         <button
-          onClick={() => {
-            handleLogout();
-            isClose();
-          }}
+          onClick={handleLogout}
           className="text-black hover:text-gray-600 px-3 py-2 rounded-md text-lg font-medium flex items-center">
           <FaUserCircle className="mr-2" />
           Logout
@@ -59,8 +50,7 @@ const MobileMenu = ({ isOpen, isLoggedIn, handleLogout, isClose }) => (
       ) : (
         <Link
           to="/login"
-          className="text-black hover:text-gray-600 px-3 py-2 rounded-md text-lg font-medium flex items-center"
-          onClick={isClose}>
+          className="text-black hover:text-gray-600 px-3 py-2 rounded-md text-lg font-medium flex items-center">
           <FaUserCircle className="mr-2" />
           Login
         </Link>
