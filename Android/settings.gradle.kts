e0e8e7e27dd1061b609e1cae904cd9f9a4e47ca2
Lenovo <vishwayatra919@gmail.com>
2024-07-12 01:30:54 +0530

    Added with the new Design

--- Android/settings.gradle.kts
+++ Android/settings.gradle.kts
@@ -16,10 +16,11 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+
         maven { url = uri("https://jitpack.io") }
     }
+
 }
 
 rootProject.name = "VigyBag"
 include(":app")
- 
