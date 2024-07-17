1beba075fcdae8fe3a5d43c759f47f6c4354d330
Vivek Prajapati <62784060+codervivek5@users.noreply.github.com>
2024-07-17 19:15:02 +0530

    fix alignment in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -11,11 +11,11 @@ import { Toaster } from "react-hot-toast";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-     <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
-    <Toaster/>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+      <Toaster />
     </Provider>
   </React.StrictMode>
 );
