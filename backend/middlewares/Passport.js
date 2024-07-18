220d90b688de643f3ebbaf4924d7c1074a9df354
Vivek Prajapati <62784060+codervivek5@users.noreply.github.com>
2024-07-18 22:19:24 +0530

    fix google auth for vigybag  domain

--- backend/middlewares/Passport.js
+++ backend/middlewares/Passport.js
@@ -10,7 +10,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/callback",
+      callbackURL: "http://vigybag.com/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
