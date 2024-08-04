eb9e8880e6768c3f5c2a4a5f21bf6f005acb4896
Garima <sushri4tech@gmail.com>
2024-08-04 23:06:20 +0530

    Add "Go To Top" button with scroll progress indicator and remove react-top-loader
    - Implemented a circular progress indicator around the "Go To Top" button to show scroll progress.
    - Updated CSS for button positioning, visibility, and hover effects.
    - The button becomes visible when the user scrolls past 2% of the page height and is positioned fixed at the bottom-right corner.
    - Removed `react-top-loader` due to compatibility issues with React versions. The previous loader implementation was replaced with a new approach using `react-loader-spinner`.

--- src/User/UserLayout.jsx
+++ src/User/UserLayout.jsx
@@ -6,36 +6,17 @@ import Footer from "./components/Footer/Footer";
 import GoToTop from "./components/GoToTop/gototop";
 import ScrollProgressBar from "./components/progressbar/ScrollProgressBar";
 import FeedbackButton from "./components/FeedbackForm/FeedBtn";
-import Loader from "./components/progressbar/Loader";
+
 const UserLayout = () => {
-  const [progress, setProgress] = useState(0);
    const location = useLocation();
 
   // Determine which navbar to show based on the current route
   const isAdminRoute = location.pathname.startsWith("/admin");
 
-  // Simulate page loading
-  useEffect(() => {
-    const simulateLoading = () => {
-      setProgress(0);
-      const interval = setInterval(() => {
-        setProgress(prev => {
-          if (prev >= 100) {
-            clearInterval(interval);
-            return 100;
-          }
-          return prev + 1;
-        });
-      }, 30); // Adjust the speed as needed
-    };
-
-    simulateLoading();
-  }, [location]);
-
   return (
     <>
       {isAdminRoute ? <AdminNavbar /> : <UserNavbar />}
-      <Loader progress={progress} /> {/*set to true to manage state as needed */}
+      
       <ScrollProgressBar />
       <Outlet />
       <Footer />
