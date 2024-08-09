113779c86ecca9163fd28af518214c0e0411d091
Sadaf <137484958+SadafKausar2025@users.noreply.github.com>
2024-08-09 18:58:35 +0530

    Created About Us page of vigybag

--- src/User/components/About/AboutUsHeader.jsx
+++ src/User/components/About/AboutUsHeader.jsx
@@ -7,15 +7,18 @@ function Header() {
       <header className="relative h-96 md:h-[100vh] flex items-center justify-center overflow-hidden">
         <div className="absolute inset-0 z-0">
           <img
-            src="https://www.shutterstock.com/image-photo/motivated-multinational-team-raise-high-600nw-1918278398.jpg"
+            src="https://www.vigybag.com/assets/Logo-HJo2K1MQ.svg"
             alt="Background"
-            className="w-full h-full object-cover filter"
+            className="w-full h-full object-cover filter blur-sm"
           />
           <div className="absolute inset-0 bg-black opacity-50"></div>
         </div>
         <div className="relative z-10 text-center text-white px-4">
+          <h1 className="text-4xl md:text-6xl font-bold mb-2 md:mb-4 text-green-400">
+            About Us
+          </h1>
           <h1 className="text-4xl md:text-6xl font-bold mb-2 md:mb-4 text-blue-400">
-            Contributors Of VigyBag
+            Welcome to VigyBag!
           </h1>
           <p className="text-lg md:text-2xl max-w-2xl mx-auto text-green-400">
             Revolutionizing eco-friendly shopping with innovative technology and
