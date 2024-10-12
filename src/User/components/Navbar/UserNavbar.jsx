1bc571d64895cc95a114e8378253db5dc98e09ef
Archisman Khanra <121884549+Archisman141@users.noreply.github.com>
2024-10-12 22:56:52 +0530

    Update UserNavbar.jsx

--- src/User/components/Navbar/UserNavbar.jsx
+++ src/User/components/Navbar/UserNavbar.jsx
@@ -117,7 +117,7 @@ const UserNavbar = ({ isAdmin }) => {
   };
 
   return (
-    <nav className="bg-[#ecd5c5] shadow-lg sticky top-0 w-full z-50 -mt-1">
+    <nav className="bg-[#ecd5c5] shadow-lg w-full z-50 -mt-1">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-20">
           <div className="flex items-center w-full">
